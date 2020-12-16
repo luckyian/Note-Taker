022d7c52fe959a989e32f2cf2deeb706d63f0be9
Ian Wren <ian.wren81@gmail.com>
2020-12-15 17:15:07 -0800

    Pulled code from server.js into apiRoutes and htmlRoutes to clean file structure.

---
+++ Develop/public/assets/routes/apiRoutes.js
@@ -0,0 +1,76 @@
+// Load data by linking routes
+const fs = require('fs');
+// var notesData = require('../db/notes.json');
+//Routing
+module.exports = function (app) {
+    // GET "/api/notes" responds with all notes from the database - db.json
+    app.get('/api/notes/', function (req, res) {
+        console.log("Inside Get routes");
+        //Read the JSON file
+        fs.readFile("./db/db.json", "utf8", (err, response) => {
+            if (err) throw err;
+            let allNotes;
+
+            // If notes isn't an array or can't be turned into one, send back a new empty array
+            try {
+                allNotes = [].concat(JSON.parse(response));
+
+            } catch (err) {
+                allNotes = [];
+            }
+
+            // return allNotes; 
+            res.json(allNotes);
+        });
+    });
+    app.post('/api/notes/', function (req, res) {
+        console.log("Inside post routes");
+        //Read the JSON file
+        fs.readFile("./db/db.json", "utf8", (err, response) => {
+
+            if (err) throw err;
+            let allNotes;
+
+            // If notes isn't an array or can't be turned into one, send back a new empty array
+            try {
+                allNotes = [].concat(JSON.parse(response));
+
+            } catch (err) {
+                allNotes = [];
+            }
+            const lastId = allNotes[allNotes.length - 1].id;
+            lastId++;
+
+            const newFile = { ...req.body, id: lastId };
+            const combinedNotes = [...allNotes, newFile]
+            fs.writeFile("./db/db.json", JSON.stringify(combinedNotes), (err) => {
+                console.log("Created new note!");
+            })
+        });
+    });
+    app.delete('/api/notes/:id', function (req, res) {
+        console.log("Inside delete routes");
+        const deleteNoteId = req.params.id
+        //Read the JSON file
+        fs.readFile("./db/db.json", "utf8", (err, response) => {
+
+            if (err) throw err;
+            let allNotes;
+
+            // If notes isn't an array or can't be turned into one, send back a new empty array
+            try {
+                allNotes = [].concat(JSON.parse(response));
+
+            } catch (err) {
+                allNotes = [];
+            }
+
+            const result = allNotes.filter(notes => notes.id != deleteNoteId);
+            // const deleteNote = [...allNotes, newFile]
+            fs.writeFile("./db/db.json", JSON.stringify(result), (err) => {
+                console.log("Deleted note!");
+                res.json(result);
+            })
+        });
+    })
+}
---
+++ Develop/public/assets/routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+//Dependencies
+var path = require('path');
+// Routing
+module.exports = function(app) {
+
+// "/notes" responds with the notes.html file
+app.get('/notes/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+// "/notes" responds with the notes.html file
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+}
