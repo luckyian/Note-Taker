93d6069dcf5c6a6aaf5847248be40ceae2b8d75c
Ian Wren <ian.wren81@gmail.com>
2020-12-15 17:12:55 -0800

    Edited code to reflect changes made to file structure.

--- server.js
+++ server.js
@@ -31,60 +31,9 @@ require("./routes/htmlRoutes")(app);
 
 
 
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM notes;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    res.render("index", { notes: data });
-  });
-});
 
 // Create a new note
-app.post("/api/notes", function(req, res) {
-  connection.query("INSERT INTO notes (note) VALUES (?)", [req.body.notes], function(err, result) {
-    if (err) {
-      return res.status(500).end();
-    }
 
-    // Send back the ID of the new notes
-    res.json({ id: result.id });
-    console.log({ id: result.id });
-  });
-});
-
-// Update a notes
-app.put("/api/notes/:id", function(req, res) {
-  connection.query("UPDATE notes SET notes = ? WHERE id = ?", [req.body.notes, req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.changedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
-
-// Delete a note
-app.delete("/api/notes/:id", function(req, res) {
-  connection.query("DELETE FROM notes WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
 
 // =============================================================================
 // LISTENER
