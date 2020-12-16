92f16dcde62fdc35d262a885489eeba8109c5bfa
Ian Wren <ian.wren81@gmail.com>
2020-12-15 17:31:15 -0800

    Fixed file tree calls to find index.html and notes.html

--- Develop/public/assets/routes/htmlRoutes.js
+++ Develop/public/assets/routes/htmlRoutes.js
@@ -5,11 +5,11 @@ module.exports = function(app) {
 
 // "/notes" responds with the notes.html file
 app.get('/notes/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
   });
 
 // "/notes" responds with the notes.html file
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
   });
 }
