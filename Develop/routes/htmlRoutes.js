27d5af874c903b52840ad52beb0e1430076d667f
Ian Wren <ian.wren81@gmail.com>
2020-12-28 15:22:00 -0800

    Moved files around to ensure heroku deployment

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -12,4 +12,8 @@ app.get('/notes/', function(req, res) {
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 }
